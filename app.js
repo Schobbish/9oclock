6e91cfbb370cbfc519cbb8cf2382b6d4d064b6e0
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2018-03-22 08:17:41 -0700

    removed tests and added beginnings of if statement

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
    var audioBox = document.getElementById("myAudio");
    var d;
-   alert(audioBox);
-   alert($('#myAudio').html());
    setInterval(function() {
       d = new Date();
       $('#Time').html(d.toLocaleTimeString());
+      if(d.getSeconds() == 0) {
+         $('#testBox').html('whole minute')
+      };
    }, 1000);
 });
