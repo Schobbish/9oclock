a0ff0f3659b243f34e40133327a40af38ea62db4
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2018-03-22 07:59:34 -0700

    Testing something

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
    var audioBox = document.getElementById("myAudio");
    var d;
+   alert(audioBox);
+   alert($('#myAudio').html());
    setInterval(function() {
       d = new Date();
       $('#Time').html(d.toLocaleTimeString());
