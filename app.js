dad522283f66ae4e56840cc41528412105a2c887
Nathan Adam <nathaniellukasadam@gmail.com>
2018-03-22 17:08:38 -0700

    made the thing run fifty times a second

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
    var audioBox = document.getElementById("audioBox");
    var d;
+
+   // this will run fifty times a second (for accuracy)
    setInterval(function() {
       d = new Date();
+      // Date(2018, 2, 17) is a Saturday
       $('#timeBox').html(d.toLocaleTimeString());
+
+      if(d.getDay() == 6) {
+         $('#dayBox').html('Today is Saturday');
+      }
       if(d.getSeconds() == 0) {
-         $('#testBox').html('whole minute')
-      };
-   }, 1000);
+         audioBox.play();
+      }
+   }, 50);
 });
