c8205dc33bd78d4bd388af1c978c2f5fe2cc0091
Nathaniel Adam <36092204+Schobbish@users.noreply.github.com>
2018-03-24 09:32:58 -0700

    create projector js

---
+++ projector/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+   /*
+   This one has no if statment and can't play the song
+   */
+   // playing this can't be done with the jquery function
+   // var audioBox = document.getElementById("audioBox");
+   var d;
+
+   // this will run fifty times a second (for accuracy)
+   setInterval(function() {
+      d = new Date();
+      $('#timeBox').html(d.toLocaleTimeString('en-us'));
+
+      /* if(d.getDay() == 6) {
+         $('#dayBox').html('Today is Saturday');
+      } else {
+         $('#dayBox').html('Today is Not Saturday');
+      } */
+      // if saturday and exactly at 9:00:00 (am or pm)
+      // correct if statement:
+      // d.getDay() == 6 && (d.getHours() == 9 || d.getHours() == 21) && d.getMinutes() == 0 && d.getSeconds() == 0
+      /* if(d.getDay() == 6 && (d.getHours() == 9 || d.getHours() == 21) && d.getMinutes() == 0 && d.getSeconds() == 0) {
+         audioBox.play();
+      } */
+   }, 20);
+});
