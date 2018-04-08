0a21a1dfd22ecf8b5735f4a28fc21bf9837b9654
Nathan Adam <nathaniellukasadam@gmail.com>
2018-04-07 20:48:26 -0700

    added multiple property functionality
    also modified comments and moved around line breaks

--- projector/app.js
+++ projector/app.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-    /*
-    This one has no if statment and can't play the song
-    */
     var d;
-
     // this will run fifty times a second (for accuracy)
     setInterval(function() {
         d = new Date();
@@ -12,20 +8,21 @@ $(document).ready(function() {
 
     // for commands in the textarea
     $('textarea').change(function() {
-        /* TEST COMMAND:
->>> h1 { color: red; font-family: "Times New Roman; font-size: 16 }
+        /* TEST COMMANDS:
+>>> h1 { color: red; font-family: "Trebuchet MS"; font-size: 32px; }
+>>> --foot { display: none }
+>>> done
         */
         // get value of textarea
         text = $(this).val();
         // split at each line
         lines = text.split('\n');
+        
         for(var i = 0; i < lines.length; i++) {
             // check if a command by splitting at the first space
             if(lines[i].split(' ', 1) == '>>>') {
                 // properties are keys and values are values
-                // this will only ever have one item but it must be done this way
                 var styles = {};
-
                 // split at the curly brace to separate the selector from the declaration
                 var selectDeclar = lines[i].split(' { ', 2);
                 // take out >>> from the first value of sd to get the selector
@@ -33,20 +30,28 @@ $(document).ready(function() {
 
                 // allows for a done command later
                 if(selector != 'done') {
-                    // split the declaration by the colon to get property and its value
-                    var propVal = selectDeclar[1].split(': ', 2);
+                    // split into individual declarations
+                    var declars = selectDeclar[1].split('; ');
+                    // take off curly brace from the last declaration
+                    var lastDeclar = declars.length - 1;
+                    declars[lastDeclar] = declars[lastDeclar].replace(' }', '');
 
-                    // assign value to property in dictionary
-                    styles[propVal[0]] = propVal[1];
+                    // for every declaration...
+                    for(var j = 0; j < declars.length; j++) {
+                        // split the declaration by the colon to get property and its value
+                        var propVal = declars[j].split(': ', 2);
+                        // assign value to property in dictionary
+                        styles[propVal[0]] = propVal[1];
+                    }
+
+                    // handle special selectors
                     if(selector == '--main') {
-                        // this selector will select #clock and textarea
                         selector = '#clock, textarea';
                     }
                     if(selector == '--foot') {
-                        // this selector will selector the footer and the paragraph and links in it
                         selector = 'footer, footer p, footer a';
                     }
-                    // use the styles dictionary to set the css
+
                     $(selector).css(styles);
                 } else {
                     // creates the done command
